269cec2ff728ba914a32bf4bb803c3cd17359425
Diana Olivas <dolivas644@gmail.com>
2022-11-10 13:51:37 -0800

    Working Home navigation bar and page placeholders

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,14 @@ import React from 'react'
 import NavBar from './auth/nav-bar';
 import { useAuth0 } from '@auth0/auth0-react';
 import Loading from './auth/loading';
-import {Route, Routes, Link } from 'react-router-dom';
+// import {Route, Routes, Link } from 'react-router-dom';
 import LoginDash from './components/Login/LoginDash';
-
+import Home from './components/Home/Home';
+import TopRanks from './components/Ranks/TopRanks';
+import Contact from './components/Contact/Contact';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import UserInfo from './components/User/UserInfo';
+import HomeNav from './components/Nav/HomeNav';
 function App() {
 
   const { isLoading } = useAuth0();
@@ -18,6 +23,18 @@ function App() {
   return (
     <div id="app" className="d-flex flex-column h-100">
     <NavBar />
+    <div className="NavBar">
+      {!user ? "":
+        <Router>
+          <HomeNav />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/TopRanks' element={<TopRanks />}></Route>
+            <Route path='/UserInfo' element={<UserInfo />}></Route>
+            <Route path='/Contact' element={<Contact />}></Route>
+          </Routes>
+        </Router> }
+      </div>
     <div className="container flex-grow-1">
     {!user ? <LoginDash/> : <Dashboard /> }
 
---
+++ client/src/components/Ranks/TopRanks.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TopRanks = () => {
+  return (
+    <div className="Header">
+    <header className="App-header">
+    This is my Top Ranks page
+    </header>
+
+</div>
+  )
+}
+
+export default TopRanks
