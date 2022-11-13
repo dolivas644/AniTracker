22bc1a3735451037295c0cad2cfedd1e0b5e3002
Diana Olivas <dolivas644@gmail.com>
2022-11-12 22:16:55 -0800

    Fixed useEffect & added contained

--- client/src/components/Ranks/TopRanks.js
+++ client/src/components/Ranks/TopRanks.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState , useEffect} from 'react';
 import AnimeCard from './cards/AnimeCard';
 import MangaCard from './cards/MangaCard';
+import PeopleCard from './cards/PeopleCard';
 
 const TopRanks = () => {
     //toggleButton Feature 
@@ -15,6 +16,7 @@ const TopRanks = () => {
         const resData =await res.json();
     
         setTopAnime(resData.data);
+        setSelected('anime')
     }
     // useEffect(()=>{
     //   getTopAnime()
@@ -29,29 +31,53 @@ const getTopManga=async()=>{
     const resData =await res.json();
 
     setTopManga(resData.data);
+    setSelected('manga')
 }
-useEffect(()=>{
-  getTopManga()
-  getTopAnime()
-},[])
+
+//  get top manga series based on popularity/ranking
+ const [topPeople, setTopPeople]=useState([]);
+
+ const getTopPeople=async()=>{
+     const res =await fetch(`https://api.jikan.moe/v4/top/people`)
+     const resData =await res.json();
+ 
+     setTopPeople(resData.data);
+     setSelected('people')
+ }
+const contentContainer = () =>{
+    if(selected === 'anime'){
+        return <AnimeCard topAnime={topAnime}/>
+    }else if (selected === 'manga'){
+        return <MangaCard topManga={topManga}/>
+    }else if (selected === 'people'){
+        return <PeopleCard topPeople={topPeople}/>
+    }
+}
+// useEffect(()=>{
+//   getTopManga()
+//   getTopAnime()
+//   getTopPeople()
+// },[])
     return (
         <div className="buttonGroup">
 
                 <div >
-                    <button className='button-17' onClick={() => setSelected("A")} >Anime</button>
-                    <button className='button-17' onClick={() => setSelected("B")}>Manga</button>
-                    <button className='button-17'>People</button>
+                    <button className='button-17' onClick={getTopAnime} >Anime</button>
+                    <button className='button-17' onClick={getTopManga}>Manga</button>
+                    <button className='button-17' onClick={getTopPeople}>People</button>
                     <button className='button-17' >Characters</button>
                     <button className='button-17'>Reviews</button>
                 </div>
                 <div className='rankContainer'>
                   {/* {selected} */}
-                  {
+                  {/* {
                     {
                         A: <div> <AnimeCard topAnime={topAnime}/></div>,
-                        B:<div> <MangaCard topManga={topManga}/></div>
+                        B:<div> <MangaCard topManga={topManga}/></div>,
+                        // C:<div> <PeopleCard topPeople={topPeople}/></div>,
                     }[selected]
-                  }
+                  } */}
+                  {contentContainer()}
                 </div>
                
                 </div>
--- client/src/components/Ranks/cards/PeopleCard.js
+++ client/src/components/Ranks/cards/PeopleCard.js
@@ -1,11 +1,38 @@
 import React from 'react'
 
-const PeopleCard = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const PeopleCard = ({ topPeople }) => {
+    return (
+        <div>
+            <h3>Top People</h3>
+            {topPeople.map(people => (
+                <>
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <h2>Name : {people.name}</h2>
+                                <img className='img' src={people.images.jpg.image_url} alt="icon"></img>
+                            </div>
+                            <div class="flip-card-back">
+                                <a
+                                    href={people.url}
+                                    target="_blank"
+                                    key={people.mal_id}
+                                    rel="noreferrer">
+                                    {people.alternate_names}
+                                </a>
+                                <p>{people.name}</p>
+                                <p>Birthday: {people.birthday}</p>
+                                <p>Favorites: {people.favorites}</p>
+                                <p>About: {people.about}</p>
+                            </div>
+                        </div>
+                    </div>
+                </>
+
+            ))}
+        </div>
+    )
 }
+
 
 export default PeopleCard
