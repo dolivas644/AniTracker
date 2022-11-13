3c6ba5bc3882110e19c45007e32693dcba772271
Diana Olivas <dolivas644@gmail.com>
2022-11-13 14:32:20 -0800

    Working fetch request based on dropdown menu

--- client/src/components/Schedule/Schedule.js
+++ client/src/components/Schedule/Schedule.js
@@ -5,19 +5,39 @@ import ScheduleCard from './ScheduleCard';
 const Schedules = () => {
 
     const [season, setSeason]=useState([]);
-
-    const getSeasonNow=async()=>{
-        const res =await fetch(`https://api.jikan.moe/v4/schedules/monday`)
+    const [day, setDay]=useState('monday')
+    const getSchedule=async()=>{
+        const res =await fetch(`https://api.jikan.moe/v4/schedules/${day}`)
         const resData =await res.json();
     
         setSeason(resData.data);
     }
     useEffect(()=>{
-      getSeasonNow()
-    },[])
+      getSchedule()
+    },[day])
 
+    const [value, setValue] = useState();
+
+    const handleChange = (e) =>{
+        setValue(e.target.value);
+        setDay(e.target.value)
+    }
   return (
     <div>
+        <div>
+            <label for="day">Choose the DAY </label>
+            <select name="day" id='day' onChange={handleChange}>
+                <option value='monday'  >Monday</option>
+                <option value='tuesday'  >Tuesday</option>
+                <option value='wednesday'>Wednesday</option>
+                <option value='thursday'>Thursday</option>
+                <option value='friday'>Friday</option>
+                <option value='other'>other</option>
+                <option value='unknown'>unknown</option>
+            </select>
+            <br></br>
+            {/* <input type="submit" value="submit" onClick={getSchedule()}></input> */}
+        </div>
       <ScheduleCard season={season}/>   
 </div>
       
