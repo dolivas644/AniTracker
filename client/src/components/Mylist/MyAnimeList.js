f13bbb296f88831128fe725d865a6d4e847113c4
Diana Olivas <dolivas644@gmail.com>
2022-11-16 12:12:48 -0800

    Attempt to fix complete list not refreshing

--- client/src/components/Mylist/MyAnimeList.js
+++ client/src/components/Mylist/MyAnimeList.js
@@ -54,9 +54,11 @@ const MyAnimeList = () => {
     });
     const content = await response.json();
     console.log(content, "update false to true")
+    alert("Your anime was moved to completed list");
   }
 
   useEffect(() => {
+
     getSavedList()
   }
     , [])
