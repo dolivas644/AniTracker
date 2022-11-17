f994c1d3794f596de78d2a38243750ce2704d8c2
Diana Olivas <dolivas644@gmail.com>
2022-11-17 15:13:53 -0800

    Defined anime_id

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -5,7 +5,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 router.get('/', async (req, res) => {
-  
+  const anime_id= req.query.anime_id;
       try {
         const itemList = await db.any(`SELECT * FROM comment WHERE anime_id=$1`, [anime_id]);
         res.send(itemList);
