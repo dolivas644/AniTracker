0946b58df3791d0624c55b16efe8229de099c36f
Diana Olivas <dolivas644@gmail.com>
2022-11-17 17:45:16 -0800

    Joined to get user info with comment

--- client/src/components/Home/Comment/CommentForm.js
+++ client/src/components/Home/Comment/CommentForm.js
@@ -1,47 +1,56 @@
 import React from 'react'
 import './Comment.css'
-import { useState , useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
-const CommentForm = ({mal_id}) => {
-    
-const [comment, setComment] = useState('');
+const CommentForm = ({ mal_id }) => {
 
-const getComments = async () =>{
+    const [comment, setComment] = useState('');
 
-    const response = await fetch(`/comments?anime_id=${mal_id}`)
-    const content = await response.json();
-    setComment(content);
-}
+    const getComments = async () => {
 
-useEffect(()=>{
-    getComments()
-},[mal_id])
-if(!comment){
-    return 'loading comments . . .'
-}
-  return (
-    <div>
-        <div className='commentSection'>
-            <div className='top-bar'>
-                <h2>Comments:</h2></div>
+        const response = await fetch(`/comments?anime_id=${mal_id}`)
+        const content = await response.json();
+        setComment(content);
+    }
+
+    useEffect(() => {
+        getComments()
+    }, [mal_id])
+    if (!comment) {
+        return 'loading comments . . .'
+    }
+    return (
+        <div>
+            <div className='commentSection'>
+                <div className='top-bar'>
+                    <h2>Comments:</h2></div>
                 <form>
                     <label>Enter your comment:
-                    <input type="text" comment="comment"/>
+                        <input type="text" comment="comment" />
                     </label>
                     <input type="submit" value="Submit"></input>
                 </form>
+            </div>
+            <div className='mapComments'>
+                {comment.map(comment => {
+                    return (
+                        <>
+                        <div className='comment'>
+                        <div className='user-info-comment'>
+                                <p>{comment.name}</p>
+                                <img src={comment.picture}></img>
+                            </div>
+                            <div>
+                            <p>{comment.Text}</p>
+                            </div>
+
+                        </div>
+                        </>
+                    )
+                })}
+            </div>
         </div>
-      <div className='mapComments'>
-{comment.map(comment =>{
-    return(
-        <>
-        <p>{comment.Text}</p>
-        </>
     )
-})}
-      </div>
-    </div>
-  )
 }
 
 export default CommentForm
