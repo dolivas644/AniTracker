69384c505825a26bbe18b0fcd5258f5621ecfc2a
Diana Olivas <dolivas644@gmail.com>
2022-11-21 14:37:08 -0800

    test home

--- client/src/__tests__/components/home.test.js
+++ client/src/__tests__/components/home.test.js
@@ -6,3 +6,25 @@ describe ("Home", () =>{
         render(<Home />);
     });
 });
+
+
+// const unmockedFetch = global.fetch;
+
+// beforeAll(() =>{
+//     global.fetch = () =>{
+//         Promise.resolve({
+//             json: () => Promise.resolve([]),
+//         })
+//     }
+// })
+
+// afterAll(() =>{
+//     global.fetch = unmockedFetch
+// })
+
+// describe('fetch', () => {
+//     test('works', async () => {
+//         expect(Array.isArray(json)).toEqual(true)
+//         expect(json.length).toEqual(0)
+//     })
+// })
