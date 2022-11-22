9dff4512e06d66e302c8ed261f312cc56c3cdfcb
Diana Olivas <dolivas644@gmail.com>
2022-11-21 22:06:44 -0800

    Delete public...

--- server/migrations/sqls/20221122052231-initial-up.sql
+++ server/migrations/sqls/20221122052231-initial-up.sql
@@ -1,26 +1,26 @@
 /* Replace with your SQL commands */
 
-CREATE TABLE public.anime (
+CREATE TABLE anime (
     mal_id integer NOT NULL,
     title text,
     image text
 );
 
-CREATE TABLE public.comment (
+CREATE TABLE comment (
     comment_id integer NOT NULL,
     "Text" text,
     user_id integer,
     anime_id integer
 );
 
-CREATE TABLE public.user_anime_list (
+CREATE TABLE user_anime_list (
     id integer NOT NULL,
     user_id integer,
     anime_id integer,
     complete boolean DEFAULT false
 );
 
-CREATE TABLE public.users (
+CREATE TABLE users (
     id integer NOT NULL,
     name character varying,
     picture character varying,
@@ -28,12 +28,12 @@ CREATE TABLE public.users (
     sub character varying
 );
 
-SELECT pg_catalog.setval('public.anime_mal_id_seq', 1, false);
+SELECT pg_catalog.setval('anime_mal_id_seq', 1, false);
 
-SELECT pg_catalog.setval('public.comment_comment_id_seq', 19, true);
+SELECT pg_catalog.setval('comment_comment_id_seq', 19, true);
 
-SELECT pg_catalog.setval('public.user_anime_list_id_seq', 61, true);
+SELECT pg_catalog.setval('user_anime_list_id_seq', 61, true);
 
-SELECT pg_catalog.setval('public.users_id_seq', 6, true);
+SELECT pg_catalog.setval('users_id_seq', 6, true);
 
-CREATE UNIQUE INDEX user_anime_unique ON public.user_anime_list USING btree (user_id, anime_id);
+CREATE UNIQUE INDEX user_anime_unique ON user_anime_list USING btree (user_id, anime_id);
